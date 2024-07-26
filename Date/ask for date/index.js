c640bbc11d6f8e246caa062cd02a413c0d3e0c24
DhruviModha22 <160628864+DhruviModha22@users.noreply.github.com>
2024-07-26 23:04:16 +0530

    Add files via upload

---
+++ Date/ask for date/index.js
@@ -0,0 +1,25 @@
+let a=prompt("Hey!!");
+
+let weatherRespone = prompt("How's the weather been treating you lately? (good/bad)");
+if(weatherRespone=="good"){
+    alert("That's awesome to hear! good weather can really brighten the day.");
+}else{
+    alert("oh no, Hopefully it improves soon!");
+}
+
+ let movieRespone=prompt("speaking of intersets, do you like movies? (yes/no)");
+
+ if(movieRespone=="yes"){
+    alert("Great! movies are such a fun way to relex and enjoy some time together.");
+ }else{
+    alert("That's okay! there are plenty of other way to have a good time.");
+ }
+
+ let dateRespone=prompt("i've really enjoyed talking with you. Would you to go see a movie with me sometime? (yes/no)");
+ if(dateRespone=="yes"){
+    alert("Awersome! I'll message you soon so we can plan it out.");
+ }else{
+    alert("No worries! Maybe we can find something else fun to do together.");
+ }
+
+ alert("thanks for chatting with me! Looking forward to hanging out soon.");
